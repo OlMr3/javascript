49f12b51645e286f1475e798a17ccc1c2393f87f
OlMr3 <olenka.mryazeva@mail.ru>
2025-01-26 19:36:42 +0300

    optimisation clean

--- home/js1/clean.js
+++ home/js1/clean.js
@@ -4,11 +4,20 @@ function removeSpace(str){
     while (start <= end && str[start] === ' '){
         start++;
     }
+    if (start>end){
+        console.log('Строка состояла только из пробелов');
+        return '';
+    }
     while (end>=start && str[end] === ' '){
         end--
+    }
+    if (start === 0 && end === str.length - 1){
+        console.log('Пробелы отсутствуют');
+        return str;
     }
     return str.substring(start, end+1);
 }
 let userStr = prompt('Введите строку');
 let res = removeSpace(userStr);
+console.log(res);
 alert(`*${res}*`);
