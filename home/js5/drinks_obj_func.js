0821dc0bcb0c69c68143fd5684c74418f173aefd
OlMr3 <olenka.mryazeva@mail.ru>
2025-03-10 11:45:10 +0300

    drink storage did

--- home/js5/drinks_obj_func.js
+++ home/js5/drinks_obj_func.js
@@ -18,3 +18,49 @@ function ObjStorageFunc() {
     }
 }
 var drinkStorage = new ObjStorageFunc;
+function add() {
+    const name = prompt('Введите имя напитка');
+    const alco = confirm('Алкогольный?');
+    let spiritsYes = 'Да';
+    let spiritsNo = 'Нет';
+    let spirits;
+    if (alco) {
+        spirits = spiritsYes;
+    }
+    else {
+        spirits = spiritsNo;
+    }
+    const recipe = prompt('Введите рецепт напитка');
+    drinkStorage.addValue(name, { a: spirits, r: recipe });
+}
+function get() {
+    const name = prompt('Введите имя напитка');
+    const info = drinkStorage.getValue(name);
+    if (!info) {
+        alert('Нет такого напитка');
+    }
+    else {
+        alert(`Напиток: ${name} 
+Алкогольный: ${info.a} 
+Рецепт приготовления: ${info.r}`);
+    }
+}
+function del() {
+    const name = prompt('Введите имя напитка, которое хотите удалить');
+    const deleteInfo = drinkStorage.deleteValue(name);
+    if (!deleteInfo) {
+        alert('Нет такого напитка');
+    }
+    else {
+        alert('Напиток удален')
+    }
+}
+function showList() {
+    const listDrinks = drinkStorage.getKeys();
+    if (listDrinks.length === 0) {
+        alert('Напитки отсутствуют');
+    }
+    else {
+        alert(listDrinks.join(', '));
+    }
+}
